f450a8a8085dfa4cf171ae02c7a0fe9e2c7d888f
gowrii23 <sgowrishankarrr@gmail.com>
2025-01-05 13:49:17 +0530

    homedecor

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => (
     <footer className="footer">
         <div className="map-container">
             <iframe
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d86332.21157351325!2d80.18249236650043!3d12.93057575243884!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a5267292c15b7bb%3A0xd37376b7f62e2ae2!2sUPTOWN%20%7C%2024%2F7%20entertainment%20centre%2C%20Kathipara%20Urban%20Square!5e0!3m2!1sen!2sin!4v1736060891311!5m2!1sen!2sin"
+                src= "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d827.501209419203!2d150.90584366963608!3d-33.94100368572696!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6b1295aa3c9bc40f%3A0xd7dca0374ffbc676!2spergolas%20and%20patios!5e0!3m2!1sen!2sin!4v1736064709911!5m2!1sen!2sin"
                 width="100"
                 height="100"
                 allowFullScreen=""
